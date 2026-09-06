1ea0c481907683fea39cd5ec46a152c3d9c5d629
Sakib Hadziavdic <sakib@sake.ba>
2026-09-06 21:02:55 +0200

    refactor: encapsulate user storage

--- blog/src/users/UsersApi.scala
+++ blog/src/users/UsersApi.scala
@@ -5,17 +5,23 @@ import blog.model.UserId
 trait UsersApi {
   def getUsers(): List[User]
   def getUser(id: UserId): Option[User] = getUsers().find(_.id == id)
+  def register(name: String): User
 }
 
 class UsersApiImpl extends UsersApi {
-  var users = List(User(UserId(1), "Alice"), User(UserId(2), "Bob"))
+  private var users = List(User(UserId(1), "Alice"), User(UserId(2), "Bob"))
+
   override def getUsers(): List[User] = users
+
+  override def register(name: String): User =
+    val nextId = users.map(_.id.value).maxOption.getOrElse(0) + 1
+    val user = User(UserId(nextId), name)
+    users = users :+ user
+    user
 }
 
-/** Bulk imports users directly by mutating the public collection. */
+/** Bulk imports users through the public API, one registration at a time. */
 object BulkUserImport {
-  def importUsers(api: UsersApiImpl, names: List[String]): Unit =
-    val startId = api.users.map(_.id.value).maxOption.getOrElse(0) + 1
-    val imported = names.zipWithIndex.map { case (name, i) => User(UserId(startId + i), name) }
-    api.users = api.users ++ imported
+  def importUsers(api: UsersApi, names: List[String]): List[User] =
+    names.map(api.register)
 }

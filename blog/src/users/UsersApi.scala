a2da24b45cee2633c15a834ac1e93b0375ce26f0
Sakib Hadziavdic <sakib@sake.ba>
2026-09-06 21:02:36 +0200

    feat: support bulk user import

--- blog/src/users/UsersApi.scala
+++ blog/src/users/UsersApi.scala
@@ -11,3 +11,11 @@ class UsersApiImpl extends UsersApi {
   var users = List(User(UserId(1), "Alice"), User(UserId(2), "Bob"))
   override def getUsers(): List[User] = users
 }
+
+/** Bulk imports users directly by mutating the public collection. */
+object BulkUserImport {
+  def importUsers(api: UsersApiImpl, names: List[String]): Unit =
+    val startId = api.users.map(_.id.value).maxOption.getOrElse(0) + 1
+    val imported = names.zipWithIndex.map { case (name, i) => User(UserId(startId + i), name) }
+    api.users = api.users ++ imported
+}

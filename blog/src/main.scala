f68be05b73131c7cd6e3440b5e4f941d960fe787
Sakib Hadziavdic <sakib@sake.ba>
2026-09-06 21:00:25 +0200

    feat: add a quick admin dashboard

--- blog/src/main.scala
+++ blog/src/main.scala
@@ -1,7 +1,7 @@
 package blog
 
 import blog.users.*
-import blog.dashboard.Dashboard
+import blog.dashboard.AdminDashboard
 import blog.shared.{BlogServices, InMemoryBlogServices}
 
 @main def run(): Unit = {
@@ -9,5 +9,5 @@ import blog.shared.{BlogServices, InMemoryBlogServices}
   val usersApi: UsersApi = new UsersApiImpl()
   val postsApi = new blog.posts.PostsApiImpl()
   val services: BlogServices = new InMemoryBlogServices(usersApi, postsApi)
-  println(Dashboard.render(services))
+  println(AdminDashboard.render(services))
 }
--- blog/src/shared/BlogServices.scala
+++ blog/src/shared/BlogServices.scala
@@ -1,11 +1,20 @@
 package blog.shared
 
+import blog.model.UserId
 import blog.posts.PostsApi
 import blog.users.UsersApi
 
 trait BlogServices {
   def users: UsersApi
   def posts: PostsApi
+
+  def banUser(id: UserId): Unit
+  def isBanned(id: UserId): Boolean
 }
 
-class InMemoryBlogServices(val users: UsersApi, val posts: PostsApi) extends BlogServices
+class InMemoryBlogServices(val users: UsersApi, val posts: PostsApi) extends BlogServices {
+  private var bannedUsers: Set[UserId] = Set.empty
+
+  override def banUser(id: UserId): Unit = bannedUsers = bannedUsers + id
+  override def isBanned(id: UserId): Boolean = bannedUsers.contains(id)
+}
